17577b26fbfa04c4e97dc65365cd3482f40adbb3
smokeyburnoutlap <gordonrdepuy@gmail.com>
2024-08-06 16:49:50 -0400

    Revert "Fix deprecated images.domains pattern"
    This reverts commit 49ce16c0dd1b382edc1ec2cef6120345776d19b9.

--- next.config.js
+++ next.config.js
@@ -24,15 +24,7 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: false,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },{
-        protocol: 'https',
-        hostname: 'source.unsplash.com',
-      },
-    ],
+    domains: ['images.unsplash.com', 'source.unsplash.com'],
   },
 };
 
