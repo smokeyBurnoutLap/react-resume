a71b2d4fb3ad54ae87533aa1397eefb1202136ab
smokeyburnoutlap <gordonrdepuy@gmail.com>
2024-12-20 04:04:45 -0500

    turned on maitenance mode

--- next.config.js
+++ next.config.js
@@ -28,7 +28,8 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-      },{
+      },
+      {
         protocol: 'https',
         hostname: 'source.unsplash.com',
       },
@@ -37,3 +38,13 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+module.exports = {
+  redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === '1'
+        ? {source: '/((?!maintenance).*)', destination: '/maintenance.html', permanent: false}
+        : null,
+    ].filter(Boolean);
+  },
+};
