dcb1ecf969173f233c7d9693030e4bbfa3394bd7
smokeyburnoutlap <gordonrdepuy@gmail.com>
2024-12-20 04:33:21 -0500

    Revert "updated baseUrl for gitlab pipeline"
    This reverts commit e746d9d6f21a8b8a1d392f2e05b3998625a5f863.

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    baseURL: 'https://gordondepuy.com',
+    baseURL: 'http://localhost:3000',
   },
 
   /* Configure projects for major browsers */
